ef81459c681c85e6155f72764d6afd7151b06a73
indresh04 <135513191+indresh04@users.noreply.github.com>
2023-07-27 15:25:32 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+const endDate ="18 July 2023 11:18 PM"
+document.getElementById("endDate").innerText =endDate;
+const inputs = document.querySelectorAll("input")
+function clock (){
+    const end = new Date(endDate);
+    const now = new Date();
+    const diff = (end-now)/1000;
+    if (diff < 0) return;
+
+    inputs[0].value=(Math.floor(diff/3600/24))
+    inputs[1].value=(Math.floor(diff/3600%24))
+    inputs[2].value=(Math.floor((diff/60)%60))
+    inputs[3].value=(Math.floor(diff%60))
+
+}
+clock()
+setInterval(
+    ()=>{
+        clock()
+    },1000
+)
+function findSum(num,sum){
+    if (num.value > 9){
+        sum += num%10
+        num = Math.floor(num/10)
+        findSum(num,sum)
+
+    }else return sum
+
+}
+console.log(findSum(76658,0))
